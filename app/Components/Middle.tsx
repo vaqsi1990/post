e2fab29bc10463fa1d103723df5c3935217e6166
vaqsi1990 <vaqsii23@gmail.com>
2026-04-23 20:01:19 +0400

    t

--- app/Components/Middle.tsx
+++ app/Components/Middle.tsx
@@ -4,11 +4,20 @@ import React from 'react';
 import Image from 'next/image';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Autoplay, Pagination } from 'swiper/modules';
-
+import { useTranslations } from "next-intl";
+import { Link } from "@/i18n/navigation";
 import 'swiper/css';
 import 'swiper/css/pagination';
 
 const Middle = () => {
+    const t = useTranslations("home");
+    const tHeader = useTranslations("header");
+    const line1 = t("heroSpeedLine1");
+    const line2 = t("heroSpeedLine2");
+    const line3 = t("heroSpeedLine3");
+    const slide2Title = t("heroSlide2Title");
+    const slide2SubtitleLine1 = t("heroSlide2SubtitleLine1");
+    const slide2SubtitleLine2 = t("heroSlide2SubtitleLine2");
   return (
     <div className="relative mt-10 h-full md:h-[1500px]">
       <Swiper
